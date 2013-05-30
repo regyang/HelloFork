7cd5d66bf2a8f157558691d035b47665afca41d1
Ricklon <rick.rickanderson@gmail.com>
2013-05-30 16:29:29 -0400

    @rianders added the HelloFork.ino script

---
+++ HelloFork.ino
@@ -0,0 +1,14 @@
+/*
+* Hello Fork Github Example Arduino Sketch
+* Just add your GitHUb account ID and I'll add you pull request to the project.
+*/
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Add your GitHub name to the code to test creating a pull request");
+  Serial.println("Hello Github from:");
+  Serial.println("Rickl0n");
+}
